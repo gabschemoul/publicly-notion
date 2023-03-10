64d89e12b0d2f695c1b9d1499ce1c64433d311f3
Gabriel Schemoul <gabschemoul@gmail.com>
2023-03-10 14:50:59 +0100

    Added the environnement variables

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -16,13 +16,13 @@ import {
 import { db } from "@/firebase/config";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyADm8WkBXG0258zXOfM3lVFdtL__JM97po",
-  authDomain: "publicly-app.firebaseapp.com",
-  projectId: "publicly-app",
-  storageBucket: "publicly-app.appspot.com",
-  messagingSenderId: "139920072271",
-  appId: "1:139920072271:web:dc29de077e2c7e9c7a543a",
-  measurementId: "G-8Z4LDT58XT",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 export default NextAuth({
