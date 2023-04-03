7f8cad1960ade2a6f269a999d894dfbc998c8551
Gabriel Schemoul <gabschemoul@gmail.com>
2023-04-03 12:32:46 +0200

    Preparing for deployment

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,5 +1,5 @@
 export const sendEmailToNewUser = async (email) => {
-  await fetch("http://localhost:3000/api/email/newUser", {
+  await fetch("https://app.publicly.so/api/email/newUser", {
     method: "POST",
     body: JSON.stringify({
       email: email,
